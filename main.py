7341b9cfbe7346f4c02f6c95de20432710f13bd3
Santosh Sadasivuni <santosh.sadasivuni@launchrack.com>
2023-09-11 23:50:58 -0700

    revert to orig config

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return 'Hello, World! <3. This is v1.0.1 of the python app and is hosted on kubernetes'
+    return 'Hello, World! <3. This is v1.0.0 of the python app and is hosted on kubernetes'
  
  
 if __name__ == "__main__":
